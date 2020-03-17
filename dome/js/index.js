56175a893ad91996c8abf04f3b8b8f1e832e3d29
xiaoming <877603359@qq.com>
2020-03-17 16:50:37 +0800

    增加对话框 信息框

--- dome/js/index.js
+++ dome/js/index.js
@@ -8,19 +8,16 @@ var ElasticLayer = function () {
   function ElasticLayer(opt) {
     _classCallCheck(this, ElasticLayer);
 
-    var type = opt.type,
-        config = opt.config; //会话类型
-
-    if (type !== 'information' && type !== 'inquiry' && type !== 'loding') {
+    this.type = opt.type;
+    if (this.type !== 'information' && this.type !== 'inquiry' && this.type !== 'loding') {
       console.log('类型填写错误 只接收loding information inquiry');
       return;
     }
 
-    this.type = type;
-    this.layerBox = document.createElement('div');
+    this.config = opt.config;
     this.body = document.body;
-
-    this.init(config); //创建元素
+    this.layerBox = document.createElement('div');
+    this.init(this.config); //创建元素
   }
 
   _createClass(ElasticLayer, [{
@@ -32,22 +29,41 @@ var ElasticLayer = function () {
       layerBox.style.opacity = '0';
       layerBox.style.zIndex = '-1';
 
-      switch (this.type) {
-        case 'loding':
-          var _config$url = config.url,
-              url = _config$url === undefined ? 'https://iknow-pic.cdn.bcebos.com/3bf33a87e950352adb732f175043fbf2b2118b9a' : _config$url,
-              _config$txt = config.txt,
-              txt = _config$txt === undefined ? '' : _config$txt,
-              _config$isClickHide = config.isClickHide,
-              isClickHide = _config$isClickHide === undefined ? false : _config$isClickHide;
+      // loding
+      if (this.type === 'loding') {
+        var _config$url = config.url,
+            url = _config$url === undefined ? 'https://iknow-pic.cdn.bcebos.com/3bf33a87e950352adb732f175043fbf2b2118b9a' : _config$url,
+            _config$txt = config.txt,
+            txt = _config$txt === undefined ? '' : _config$txt,
+            _config$isClickHide = config.isClickHide,
+            isClickHide = _config$isClickHide === undefined ? false : _config$isClickHide;
+        //注册点击隐藏loding事件
 
+        isClickHide && layerBox.addEventListener('click', this.fadeOut.bind(this, 400));
+        //添加node
+        layerBox.innerHTML = ' <div class="ly-loding">\n                                  <div class="loding-img-box">\n                                    <img src="' + url + '" alt="">\n                                  </div>\n                                  <p>' + txt + '</p>\n                                </div>';
+      }
 
-          isClickHide && layerBox.addEventListener('click', this.fadeOut.bind(this, 400));
+      // information 信息框
+      if (this.type === 'information') {
+        var _txt = config.txt,
+            success = config.success;
 
-          layerBox.innerHTML = ' <div class="layerBox-loding">\n                                  <div class="loding-img-box">\n                                    <img src="' + url + '" alt="">\n                                  </div>\n                                  <p>' + txt + '</p>\n                                </div>';
-          break;
-        default:
-          break;
+        layerBox.innerHTML = '<div class="ly-information"">\n                                <p>' + _txt + '</p>\n                                <div class="ly-info-btn" id="ly-info-btn">\u6309\u94AE</div>\n                              </div>';
+
+        layerBox.getElementsByClassName('ly-info-btn')[0].onclick = success;
+      }
+
+      // inquiry 询问框
+      if (this.type === 'inquiry') {
+        var _txt2 = config.txt,
+            _success = config.success,
+            error = config.error;
+
+        layerBox.innerHTML = '<div class="ly-inquiry">\n                              <p>' + _txt2 + '</p>\n                              <div class="ly-inquiry-btns">\n                                <div class="ly-inquiry-success">\u786E\u5B9A</div>\n                                <div class="ly-inquiry-error">\u53D6\u6D88</div>\n                              </div>\n                            </div>';
+        console.log(layerBox);
+        layerBox.getElementsByClassName('ly-inquiry-success')[0].onclick = _success;
+        layerBox.getElementsByClassName('ly-inquiry-error')[0].onclick = error;
       }
 
       this.body.appendChild(this.layerBox);
@@ -67,7 +83,7 @@ var ElasticLayer = function () {
     value: function fadeOut(hideTime) {
       var layerBox = this.layerBox,
           hideT = hideTime || 400;
-      console.log(hideTime);
+
       layerBox.style.transition = 'opacity ' + hideT / 1000 + 's';
       layerBox.style.opacity = '0';
       setTimeout(function () {
